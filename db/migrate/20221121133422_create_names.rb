a31f75a13d1324d977fddf2ad4412a16add81f09
omran rashidy <omranrashidy@gmail.com>
2022-11-21 16:29:09 +0100

    show-page

--- db/migrate/20221121133422_create_names.rb
+++
@@ -1,9 +0,0 @@
-class CreateNames < ActiveRecord::Migration[7.0]
-  def change
-    create_table :names do |t|
-      t.integer :age
-
-      t.timestamps
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -27,12 +27,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_140834) do
     t.index ["offer_id"], name: "index_bookings_on_offer_id"
   end
 
-  create_table "names", force: :cascade do |t|
-    t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "offers", force: :cascade do |t|
     t.string "name"
     t.string "description"

39ba464c73112173dbc8bec4df2ea9c09b4c9704
Livia C <lcebotarescu@gmail.com>
2022-11-21 22:25:23 +0100

    Index page open to everyon

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,9 @@
 class OffersController < ApplicationController
-  before_action :set_offer, only: %i[ show edit update destroy ]
+  before_action :set_offer, only: %i[show edit update destroy]
+
+  # Add here the methods that you want a visitor to access without an account
+  skip_before_action :authenticate_user!, only: %i[show index]
+
   def index
     @offers = Offer.all
   end

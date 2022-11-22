475a0067392face13312ec1421d6115a6cb8f3c3
omran rashidy <omranrashidy@gmail.com>
2022-11-22 13:02:33 +0100

    The cloudinary is working

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,5 @@
 class Offer < ApplicationRecord
   has_many :bookings
   belongs_to :user
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
 
-Offer.create(
+puts 'cleaning the db..'
+Offer.destroy_all
+User.destroy_all
+
+omran = User.create!(email: 'onram@onram.com', password: 'password')
+
+offer_1_file = URI.open("https://www.startupvalley.news/de/wp-content/uploads/sites/2/2017/10/WeiterGeben.jpg")
+
+offer1 = Offer.create!(
   name: "Raclette stove",
   description: "Perfect stove for your next Raclette gathering in winter",
   price: 13.90,
-  address: "Kreuzberg"
+  address: "Kreuzberg",
+  user: omran
 )
 
-Offer.create(
+offer1.photo.attach(io: offer_1_file, filename: "nes.png", content_type: "image/png")
+offer1.save
+
+
+Offer.create!(
   name: "Drill",
   description: "Want to finally hang up the painting that's been on the floor for the past few months?",
   price: 23.90,
-  address: "Schoeneberg"
+  address: "Schoeneberg",
+  user: omran
 )
 
-Offer.create(
+Offer.create!(
   name: "Kaercher window cleaner",
   description: "Want to have the 'Durchblick' again and actually see what's outside?",
   price: 3.90,
-  address: "Mitte"
+  address: "Mitte",
+  user: omran
 )

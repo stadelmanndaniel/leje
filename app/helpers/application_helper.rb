1130718bf1d19b3af677c8549a962e8f322af49d
Livia C <lcebotarescu@gmail.com>
2022-11-25 10:02:56 +0100

    Fixed calendar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,16 @@ module ApplicationHelper
     "-" if offer.bookings.empty?
     rating = []
     offer.bookings.each { |booking| rating << booking.rating if booking.rating}
-    avg = rating.inject(:+).to_f / offer.bookings.count
+    avg = rating.inject(:+).to_f / rating.count
+    avg = "-" if rating.empty?
     return avg
+  end
+
+  def calculate_nr_reviews(offer)
+    "-" if offer.bookings.empty?
+    rating = []
+    offer.bookings.each { |booking| rating << booking.rating if booking.rating}
+    nr = rating.count
+    return nr
   end
 end
